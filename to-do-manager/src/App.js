e14bc79850d5bb4704e465ec80c57322d7eebe73
Lex A <axelemanuelpavon@gmail.com>
2022-10-04 03:45:37 -0300

    Deploy netlify

--- to-do-manager/src/App.js
+++ to-do-manager/src/App.js
@@ -5,21 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          To Do Manager
+        </h1>
       </header>
     </div>
   );
 }
 
-export default App;
+export { App };
