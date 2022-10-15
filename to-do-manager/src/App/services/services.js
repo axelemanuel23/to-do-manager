83aecf0f485eea0c556e741f7e7c0098f9c25849
Lex A <axelemanuelpavon@gmail.com>
2022-10-15 02:35:37 -0300

    creando el contexto

--- to-do-manager/src/App/services/services.js
+++ to-do-manager/src/App/services/services.js
@@ -1,18 +1,18 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-async function getTodos(apikey){
-    try{
-      const res = await axios.get("https://exampleeapp.herokuapp.com/api/v1/todomanager", {
-        headers: {
-          "APIKEY": apikey
-        }
-      })
-      const todos = res.data.data;
-      console.log(todos);
-      return todos;
-    }catch(err){
-      console.log(err)
-    }
-}
+// async function getTodos(apikey){
+//     try{
+//       const res = await axios.get("https://exampleeapp.herokuapp.com/api/v1/todomanager", {
+//         headers: {
+//           "APIKEY": apikey
+//         }
+//       })
+//       const todos = res.data.data;
+//       console.log(todos);
+//       return todos;
+//     }catch(err){
+//       console.log(err)
+//     }
+// }
 
-module.exports = { getTodos }
+// module.exports = { getTodos }
